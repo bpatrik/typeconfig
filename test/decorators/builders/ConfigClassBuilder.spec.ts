83afc8a821615b19d668113ea68f2a58b1bfad8c
Patrik J. Braun <bra.patrik@gmail.com>
2020-01-27 13:03:32 +0100

    improving tests

---
+++ test/decorators/builders/ConfigClassBuilder.spec.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import {ConfigClass} from '../../../src/decorators/class/ConfigClass';
+import {ConfigProperty} from '../../../src/decorators/property/ConfigPropoerty';
+import {ConfigClassBuilder} from '../../../src/decorators/builders/ConfigClassBuilder';
+
+const chai: any = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const should = chai.should();
+
+describe('ConfigClassBuilder', () => {
+
+  it('should build', () => {
+
+    @ConfigClass()
+    class C {
+      @ConfigProperty()
+      num: number = 5;
+    }
+
+    const c1 = ConfigClassBuilder.attachPrivateInterface(new C());
+    const c2 = ConfigClassBuilder.attachInterface(new C());
+    const c3 = ConfigClassBuilder.buildPrivate(C);
+    const c4 = ConfigClassBuilder.build(C);
+    chai.expect(c1.num).to.deep.equal(c2.num);
+    chai.expect(c1.num).to.deep.equal(c3.num);
+    chai.expect(c1.num).to.deep.equal(c4.num);
+    chai.expect(c1.toJSON()).to.deep.equal({num:5});
+    chai.expect(c2.toJSON()).to.deep.equal({num:5});
+    chai.expect(c3.toJSON()).to.deep.equal({num:5});
+    chai.expect(c4.toJSON()).to.deep.equal({num:5});
+    chai.expect(c1.__defaults).to.deep.equal({num:5});
+    chai.expect(c3.__defaults).to.deep.equal({num:5});
+  });
+
+
+});
